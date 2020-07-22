424e94673e5183f6786c8fda2a293d52a7e8ac0d
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-07-22 19:35:19 -0300

    create Board Component

---
+++ src/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container h-100 p-5">
+    <div class="row h-100">
+      <div class="col">
+        <div class="border rounded h-100">
+          <Toolbar />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Toolbar from "./Toolbar";
+export default {
+  components: { Toolbar },
+};
+</script>
+
+<style></style>

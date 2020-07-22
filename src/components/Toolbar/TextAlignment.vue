f7054d833b42112d1042502789ef91eda8af2937
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-07-22 19:35:41 -0300

    create TextAlignment Component

---
+++ src/components/Toolbar/TextAlignment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-secondary"><AlignLeft /></button>
+      <button type="button" class="btn btn-secondary"><AlignCenter /></button>
+      <button type="button" class="btn btn-secondary"><AlignRight /></button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AlignCenter from "vue-material-design-icons/FormatAlignCenter.vue";
+import AlignLeft from "vue-material-design-icons/FormatAlignLeft.vue";
+import AlignRight from "vue-material-design-icons/FormatAlignRight.vue";
+
+export default {
+  components: {
+    AlignCenter,
+    AlignLeft,
+    AlignRight,
+  },
+};
+</script>
+
+<style></style>

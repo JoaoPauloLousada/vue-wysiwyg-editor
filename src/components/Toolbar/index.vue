25037fbbe0165c4508b036a15f4d9b64e6c934fe
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-07-22 19:35:28 -0300

    create Toolbar Component

---
+++ src/components/Toolbar/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-3 bg-light d-flex">
+    <div class="mr-2">
+      <TextDecorator />
+    </div>
+    <div>
+      <TextAlignment />
+    </div>
+  </div>
+</template>
+
+<script>
+import TextDecorator from "./TextDecorator";
+import TextAlignment from "./TextAlignment";
+
+export default {
+  components: { TextDecorator, TextAlignment },
+};
+</script>
+
+<style></style>
